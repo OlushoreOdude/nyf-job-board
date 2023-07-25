3c89a08c0a0b27ead594dd0b2bf1b50a7d250327
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2023-07-25 14:47:48 +0100

    added test route for jobs page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/jobs" element={<Jobs />} />;
 	</Routes>
 );
 
