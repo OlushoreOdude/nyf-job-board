036ba3e871b29d1c76897c14045c84e58dcd00f4
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2023-07-25 13:31:38 +0100

    added route for json data

--- server/api.js
+++ server/api.js
@@ -2,6 +2,12 @@ import { Router } from "express";
 
 import logger from "./utils/logger";
 
+/*
+// route to test locally
+import jobsData from "./sampleData/sampleData.json"; */
+
+// import data from data.json at root.
+import jobsData from "../data.json";
 const router = Router();
 
 router.get("/", (_, res) => {
@@ -9,4 +15,8 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello-team+Sana, world23!" });
 });
 
+// route for jobsData
+router.get("/jobs", (_, res) => {
+  res.json({ data: jobsData });
+});
 export default router;
