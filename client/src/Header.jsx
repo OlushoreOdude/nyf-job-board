7236530a45964dcfd0c0b7a99192e6202a5a32c3
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-07-25 20:25:08 +0100

    Header.jsx created

---
+++ client/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			{/* Header content goes here */}
+			<h1>Welcome to NYF React App</h1>
+		</header>
+	);
+};
+
+export default Header;
