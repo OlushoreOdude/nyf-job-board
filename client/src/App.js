dd2e7a5006ea764db76006b13cca60421aa58248
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-07-26 19:33:23 +0100

    Header and Footer added to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,19 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Jobs from "./pages/Jobs";
 
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/jobs" element={<Jobs />} />;
-	</Routes>
+	<>
+		<Header />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about/this/site" element={<About />} />
+			<Route path="/jobs" element={<Jobs />} />;
+		</Routes>
+		<Footer />
+	</>
 );
 
 export default App;
