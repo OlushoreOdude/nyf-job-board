30f080e7c88f85cb09d0960dc56f0ec018024465
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2023-07-29 01:54:34 +0100

    updated router to use new features of v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,34 @@
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from "react-router-dom";
 
+// import Layouts //
+import RootLayout from "./layouts/RootLayout.jsx";
+
+// import pages //
 import About from "./pages/About";
-import Home from "./pages/Home";
+
+/* import Home from "./pages/Home"; */
 import Jobs from "./pages/Jobs";
+import NotFound from "./pages/NotFound";
+import HomeCopy2 from "./pages/Home_copy2.js";
 
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-
-const App = () => (
-	<>
-		<Header />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/about/this/site" element={<About />} />
-			<Route path="/jobs" element={<Jobs />} />;
-		</Routes>
-		<Footer />
-	</>
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<HomeCopy2 />}></Route>
+			<Route path="/about" element={<About />} />
+			<Route path="/jobstest" element={<Jobs />} />;
+			<Route path="*" element={<NotFound />} />
+		</Route>
+	)
 );
+
+function App() {
+	return <RouterProvider router={router} />;
+}
 
 export default App;
