0abf45d6a719dfbf69e2e3e8378f3556604b5148
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-08-04 19:26:14 +0100

    Applied gGeneral styling to Home.js/Header.js/Footer.js

--- client/src/components/JobCardContainer.jsx
+++ client/src/components/JobCardContainer.jsx
@@ -3,18 +3,16 @@ import React from "react";
 const JobCardContainer = ({ jobs }) => {
 
   return (
-    <>
-      <ul>
-        {jobs.map((job) => (
-        // line 9 to 13 should be a seperate component called JobCard.jsx to display one job
-          <li key={job.slug}>
-            <strong>Title:</strong> {job.title} <br />
-            <strong>Location:</strong> {job.location}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+		<div>
+			{jobs.map((job) => (
+				// line 9 to 13 should be a separate component called JobCard.jsx to display one job
+				<div key={job.slug}>
+					<strong>Title:</strong> {job.title} <br />
+					<strong>Location:</strong> {job.location}
+				</div>
+			))}
+		</div>
+	);
 };
 
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,52 +1,60 @@
-import Search  from "../components/Search.jsx";
+import Search from "../components/Search.jsx";
 import JobCardContainer from "../components/JobCardContainer.jsx";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import "../global.css";
 import "./Home.css";
 
 function Home() {
- const [loading, setLoading] = useState(true);
- const [error, setError] = useState(null);
- const [jobsData, setJobsData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [jobsData, setJobsData] = useState([]);
 
-useEffect(() => {
-	// Fetch data from server
-	const fetchJobs = async () => {
-		try {
-			const response = await axios.get("/api/jobs");
-			console.log(response);
-			setJobsData(response.data.data.slice(0, 10)); // Only take the first 10 jobs
-			setLoading(false);
-		} catch (error) {
-			console.log(error);
-			setError("Failed to fetch jobs data from the server");
-			setLoading(false);
-		}
-	};
+	useEffect(() => {
+		// Fetch data from server
+		const fetchJobs = async () => {
+			try {
+				const response = await axios.get("/api/jobs");
+				console.log(response);
+				setJobsData(response.data.data.slice(0, 10)); // Only take the first 10 jobs
+				setLoading(false);
+			} catch (error) {
+				console.log(error);
+				setError("Failed to fetch jobs data from the server");
+				setLoading(false);
+			}
+		};
 
-	fetchJobs();
-}, []);
+		fetchJobs();
+	}, []);
 
-if (loading) {
-	return <p>Loading...</p>;
-}
+	if (loading) {
+		return <p>Loading...</p>;
+	}
 
-if (error) {
-	return <p>{error}</p>;
-}
+	if (error) {
+		return <p>{error}</p>;
+	}
 
-if (!jobsData || jobsData.length === 0) {
-	return <p>No jobs data available.</p>;
-}
+	if (!jobsData || jobsData.length === 0) {
+		return <p>No jobs data available.</p>;
+	}
 	return (
 		<main role="main" className="main-content">
-			<div>
-				<h1 className="message" data-qa="message">
-					Jobs Board
-				</h1>
-				<Search />
-				<JobCardContainer jobs={jobsData} />
+			{/* First Column: Job Search */}
+			<div className="job-search-container">
+				<Search className="job-search-input" />
+			</div>
+
+			{/* Second Column: Job Card Container which will show 10 job card*/}
+			<div className="job-card-container">
+				<JobCardContainer jobs={ jobsData } />
+			</div>
+
+			{/* Third Column: Selected Job Details */}
+			<div className="selected-job-container">
+						<h4>No job is selected</h4>
 			</div>
 		</main>
 	);
