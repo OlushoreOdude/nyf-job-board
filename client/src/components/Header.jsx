ed7427409390f0946db3391f85c5185547b31b92
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2023-08-04 14:13:11 +0100

    added nav and searchDb page with link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<header className="header-container">
 			<h1 className="header-job-board">Job Board</h1>
+			<nav className="header-nav">
+				<NavLink exact="true" to="/">
+					Home
+				</NavLink>
+				<NavLink to="/searchjobsdb">SearchDb</NavLink>
+			</nav>
 		</header>
 	);
 };
