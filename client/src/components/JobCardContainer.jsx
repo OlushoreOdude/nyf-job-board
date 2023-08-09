a4b59d02244ce5ecdd537946c722051a642006d1
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-08-09 17:30:30 +0100

    JobCardContainer.css created and basic styling added

--- client/src/components/JobCardContainer.jsx
+++ client/src/components/JobCardContainer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+
+import "./JobCardContainer.css";
 import JobCard from "./JobCard.jsx";
 
 const JobCardContainer = ({ jobs, onJobClick }) => {
 	return (
-		<div>
+		<div className="job-card-container">
 			<JobCard jobs={jobs} onJobClick={onJobClick} />
 		</div>
 	);
