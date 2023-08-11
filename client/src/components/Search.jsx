5c32f626bb5701f5d87bee37fa0e9bfaaa77f5fa
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-08-11 17:21:05 +0100

    Create a Show recent jobs only checkbox and Search.css

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,60 +1,81 @@
 import React, { useState, useEffect } from "react";
 
+import "./Search.css";
+
 const Search = () => {
-  const [result, setResult] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
-  const [filteredResult, setFilteredResult] = useState([]);
+	const [result, setResult] = useState([]);
+	const [searchInput, setSearchInput] = useState("");
+	const [filteredResult, setFilteredResult] = useState([]);
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
+	const [showRecent, setShowRecent] = useState(false); // State for the checkbox
 
-  const fetchJobs = () => {
-    fetch("/api/jobs")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        return res.json();
-      })
-      .then((body) => {
-        setResult(body.data.jobs);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+	useEffect(() => {
+		fetchJobs();
+	}, []);
 
-  const filterByTitle = () => {
-    const filteredJobs = result.filter(
-      (job) => job.job_title.toLowerCase().includes(searchInput.toLowerCase())
-    );
-    setFilteredResult(filteredJobs);
-  };
+	const fetchJobs = () => {
+		fetch("/api/jobs")
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((body) => {
+				setResult(body.data.jobs);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
 
-  const handleSearchInputChange = (e) => {
-    setSearchInput(e.target.value);
-  };
+	const filterByTitle = () => {
+		const filteredJobs = result.filter((job) =>
+			job.job_title.toLowerCase().includes(searchInput.toLowerCase())
+		);
 
-  return (
-    <>
-      <div>
-        <h4>Search for job by title</h4>
-        <input
-          className="job-input"
-          placeholder="Job title..."
-          value={searchInput}
-          onChange={handleSearchInputChange}
-        />
-        <button className="btn-search" onClick={filterByTitle}>
-          Search
-        </button>
-        
-      </div>
+		// Apply checkbox filtering if showRecent is true
+		const filteredJobsByDate = showRecent
+			? filteredJobs.filter((job) => true)
+			: filteredJobs; // This should be modified to filter the jobs by date once we have our DB running
 
-      
-    </>
-  );
+		setFilteredResult(filteredJobsByDate);
+	};
+
+	const handleSearchInputChange = (e) => {
+		setSearchInput(e.target.value);
+	};
+
+	const handleShowRecentChange = (e) => {
+		setShowRecent(e.target.checked);
+	};
+
+	return (
+		<div className="search-section">
+			<h4>Search jobs</h4>
+			<input
+				className="job-input"
+				placeholder="Job title..."
+				value={searchInput}
+				onChange={handleSearchInputChange}
+			/>
+			<button className="btn-search" onClick={filterByTitle}>
+				Search
+			</button>
+
+			{/* Checkbox for recent jobs */}
+			<div className="checkbox-container">
+				<label>
+					<input
+						type="checkbox"
+						checked={showRecent}
+						onChange={handleShowRecentChange}
+					/>
+					Only show recent jobs
+				</label>
+			</div>
+		</div>
+	);
 };
 
 export default Search;
