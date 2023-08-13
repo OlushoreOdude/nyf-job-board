cd2425cfe2abb337e25dc798861c9daf5cae9127
Ali <113782230+ali1373107@users.noreply.github.com>
2023-08-13 21:30:41 +0100

    118-highlite the selected job card

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,39 +1,48 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 
+import PropTypes from "prop-types";
 import "./JobCard.css";
 
 const JobCard = ({ jobs, onJobClick }) => {
-	const handleKeyPress = (event, job) => {
-		if (event.key === "Enter") {
-			onJobClick(job);
-		}
-	};
+    const [selectedJobSlug, setSelectedJobSlug] = useState(jobs[0]?.slug);
 
-	return (
-		<div>
-			{jobs.map((job) => (
-				<div
-					key={job.slug}
-					className="job-card"
-					onClick={() => onJobClick(job)}
-					onKeyDown={(event) => handleKeyPress(event, job)}
-					tabIndex={0}
-					role="button"
-				>
-					<div className="job-title">{job.title}</div>
-					<div className="company-name">{job.company_name}</div>
-					<div className="location">{job.location}</div>
-					<button className="job-details-btn">View Details</button>
-				</div>
-			))}
-		</div>
-	);
+    const handleCardClick = (job) => {
+        setSelectedJobSlug(job.slug);
+        onJobClick(job);
+    };
+
+    const handleKeyPress = (event, job) => {
+        if (event.key === "Enter") {
+            handleCardClick(job);
+        }
+    };
+
+    return (
+        <div>
+            {jobs.map((job) => (
+                <div
+                    key={job.slug}
+                    className={`job-card ${
+                        selectedJobSlug === job.slug ? "highlighted" : ""
+                    }`}
+                    onClick={() => handleCardClick(job)}
+                    onKeyDown={(event) => handleKeyPress(event, job)}
+                    tabIndex={0}
+                    role="button"
+                >
+                    <div className="job-title">{job.title}</div>
+                    <div className="company-name">{job.company_name}</div>
+                    <div className="location">{job.location}</div>
+                    <button className="job-details-btn">View Details</button>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 JobCard.propTypes = {
-	jobs: PropTypes.array.isRequired,
-	onJobClick: PropTypes.func.isRequired,
+    jobs: PropTypes.array.isRequired,
+    onJobClick: PropTypes.func.isRequired,
 };
 
 export default JobCard;
