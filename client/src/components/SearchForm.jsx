0de9957b78e9888b72dc3c8ed70fb28569aad6c4
Ali <113782230+ali1373107@users.noreply.github.com>
2023-08-13 21:16:18 +0100

    SearchForm.css created and initial styling was implemented

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -1,89 +1,81 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
+
+import "./SearchForm.css";
 
 const SearchForm = (props) => {
-  const { onClick }  = props;
-  /*const handeleOnSubmit =(event) =>{
+  const { onClick } = props;
+  const [selectedJobTypes, setSelectedJobTypes] = useState([]);
+
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    if (selectedJobTypes.includes(value)) {
+      setSelectedJobTypes(selectedJobTypes.filter((type) => type !== value));
+    } else {
+      setSelectedJobTypes([...selectedJobTypes, value]);
+    }
+  };
+
+  const handeleOnSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target.elements.title.value);
-    console.log(event.target.elements.location.value);
-    console.log(event.target.elements.job_type.value);
-    const { title,location,job_type } = event.target.elements;
+    const { title, location } = event.target.elements;
     onClick({
       title: title.value,
       location: location.value,
-      job_type: job_type.value,
-    });*/
-
-    const [selectedJobTypes, setSelectedJobTypes] = useState([]);
-
-    const handleCheckboxChange = (event) => {
-      const value = event.target.value;
-      if (selectedJobTypes.includes(value)) {
-        setSelectedJobTypes(selectedJobTypes.filter((type) => type !== value));
-      } else {
-        setSelectedJobTypes([...selectedJobTypes, value]);
-      }
-    };
-
-    const handeleOnSubmit = (event) => {
-      event.preventDefault();
-      const { title, location } = event.target.elements;
-      onClick({
-        title: title.value,
-        location: location.value,
-        job_type: selectedJobTypes.join(", "), // Convert array to comma-separated string
-      });
-    };
+      job_type: selectedJobTypes.join(", "), // Convert array to comma-separated string
+    });
+  };
   return (
     <>
-      <div>
-        <h4>Search form</h4>
-        <form onSubmit={handeleOnSubmit}>
+      <div className="search-form-container">
+        <h4 className="search-heading">Search jobs</h4>
+        <form onSubmit={handeleOnSubmit} className="search-form">
           <input
             name="title"
             type="text"
             className="job-input"
             placeholder="Job title..."
           />
+          <h4 className="search-heading">Search filters</h4>
+          <h5>Search by location</h5>
           <input
             name="location"
             type="text"
             className="job-input"
             placeholder="Location..."
           />
-            <div>
-              <label>
+          <div>
+            <label>
               Job Type:
-                Hybride
-                <input
-                  type="checkbox"
-                  value="hybrid"
-                  checked={selectedJobTypes.includes("hybrid")}
-                  onChange={handleCheckboxChange}
-                />
-              </label>
-              <label>
+              Hybride
+              <input
+                type="checkbox"
+                value="hybrid"
+                checked={selectedJobTypes.includes("hybrid")}
+                onChange={handleCheckboxChange}
+              />
+            </label>
+            <label>
               Remote
-                <input
-                  type="checkbox"
-                  value="remote"
-                  checked={selectedJobTypes.includes("remote")}
-                  onChange={handleCheckboxChange}
-                />
-              </label>
-              <label>
+              <input
+                type="checkbox"
+                value="remote"
+                checked={selectedJobTypes.includes("remote")}
+                onChange={handleCheckboxChange}
+              />
+            </label>
+            <label>
               Onsite
-                <input
+              <input
                 type="checkbox"
                 value="onsite"
                 checked={selectedJobTypes.includes("onsite")}
                 onChange={handleCheckboxChange}
-                />
-              </label>
-            </div>
-          <button className="btn-search" type="submit">
-            Search
-          </button>
+              />
+            </label>
+          </div>
+          <button className="btn-show-results" type="submit">
+          SHOW RESULTS
+        </button>
         </form>
       </div>
     </>
