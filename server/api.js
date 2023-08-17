2d4f6775814e673f0f8b35f9269a0165a29d05e2
Ali <113782230+ali1373107@users.noreply.github.com>
2023-08-17 18:16:00 +0100

    api routers are working and data displayes on the pages

--- server/api.js
+++ server/api.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import axios from "axios";
+
 import dataJobs from "./utils/SampleData/transformRes_Arbeitnow.json";
 
 // import data from data.json at root.
@@ -13,19 +13,11 @@ router.get("/", (_, res) => {
 	res.json({ message: "JOBS BOARD" });
 });
 
-// Route to fetch jobs data from the external API
+
 
 router.get("/jobs", async (_, res) => {
   try {
-    const config = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: "https://www.arbeitnow.com/api/job-board-api",
-      headers: {},
-    };
-
-    const response = await axios(config);
-    res.json(response.data);
+      res.json({ data : dataJobs });
   } catch (error) {
     // console.log(error);
     res.status(500).json({ error: "Failed to fetch jobs data from external API" });
